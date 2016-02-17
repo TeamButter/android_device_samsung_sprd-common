1c0d2f2a55ae3101def59cdec2ea8036922f01ff
corphish <d97.avinaba@gmail.com>
2016-02-17 21:55:47 +0530

    Initial Commit : Decouple from mint2g tree

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,77 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Board
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+COMMON_GLOBAL_CFLAGS += -DSPRD_HARDWARE
+
+# UMS
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/dwc_otg.0/gadget/lun0/file"
+
+# Graphics
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/samsung/sprd-common/egl/egl.cfg
+BOARD_EGL_NEEDS_FNW := true
+
+# Camera
+USE_CAMERA_STUB := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/sprd-common/bluetooth/libbt_vndcfg.txt
+
+
+# Healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.sprd
+
+# RIL
+#BOARD_RIL_CLASS := ../../../device/samsung/mint2g/ril/
+BOARD_MOBILEDATA_INTERFACE_NAME := "rmnet0"
+BOARD_RIL_NO_CELLINFOLIST := true
+
+# Audio
+BOARD_USES_TINYALSA_AUDIO := true
+
+
+# HWComposer
+USE_SPRD_HWCOMPOSER := true
+
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+
+
+# CMHW
+BOARD_HARDWARE_CLASS := device/samsung/sprd-common/cmhw/
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/sprd-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts
---
+++ device.mk
@@ -0,0 +1,100 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH := device/samsung/sprd-common
+
+# Media
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+     $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
+
+
+#Wifi
+PRODUCT_PACKAGES += \
+	dhcpcd.conf \
+	wpa_supplicant \
+	hostapd \
+	wpa_supplicant.conf
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    tinymix
+    
+
+PRODUCT_PACKAGES += \
+	Gallery2
+
+
+# Board-Pecific
+PRODUCT_PACKAGES += \
+    lights.sc8810 \
+    sensors.sc8810
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    setup_fs
+
+# Misc packages
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+    
+# Samsung Service Mode
+PRODUCT_PACKAGES += \
+    SamsungServiceMode
+    
+# OMX
+PRODUCT_PACKAGES += \
+	libomx_aacdec_sprd.so \
+	libomx_apedec_sprd.so \
+	libomx_avcdec_hw_sprd.so \
+	libomx_avcdec_sw_sprd.so \
+	libomx_avcenc_hw_sprd.so \
+	libomx_m4vh263dec_hw_sprd.so \
+	libomx_m4vh263dec_sw_sprd.so \
+	libomx_m4vh263enc_hw_sprd.so \
+	libomx_mp3dec_sprd.so \
+	libomx_vpxdec_hw_sprd.so
+    
+# Web
+PRODUCT_PACKAGES += \
+    libskia_legacy
+
