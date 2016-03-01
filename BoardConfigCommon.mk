a181bdec5ca0607139800e71d2c7e716bf1ff3aa
corphish <d97.avinaba@gmail.com>
2016-03-01 23:21:41 +0530

    sprd: Fixup ril class path
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,7 +48,7 @@ BOARD_BLUEDROID_VENDOR_CONF := device/samsung/sprd-common/bluetooth/libbt_vndcfg
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.sprd
 
 # RIL
-BOARD_RIL_CLASS := device/samsung/sprd-common/ril/
+BOARD_RIL_CLASS := ../../../device/samsung/sprd-common/ril/
 BOARD_MOBILEDATA_INTERFACE_NAME := "rmnet0"
 
 # Audio
