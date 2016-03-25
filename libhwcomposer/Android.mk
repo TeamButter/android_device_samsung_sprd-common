29ade005f10166126947abc3c07cafc66eb87b0b
corphish <d97.avinaba@gmail.com>
2016-03-25 20:00:56 +0530

    Revert "hwc: Update to 0.3"
    This reverts commit 10ad2e97a94b3c6f23fa92f21e9f8c28dea0a261.

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
 
 # HAL module implemenation stored in
@@ -19,9 +20,8 @@ include $(CLEAR_VARS)
 ifeq ($(strip $(USE_SPRD_HWCOMPOSER)),true)
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libEGL libmemoryheapion libbinder libutils libcutils libUMP libGLESv1_CM libhardware libui
+LOCAL_SHARED_LIBRARIES := liblog libEGL libbinder libmemoryheapion libutils libcutils libUMP libGLESv1_CM libhardware libui
 LOCAL_SRC_FILES := hwcomposer.cpp \
-		   vsync/vsync.cpp \
                    dump_bmp.cpp
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/../libgralloc \
@@ -30,53 +30,32 @@ LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/../kernel_headers \
 	$(LOCAL_PATH)/vsync \
 	$(LOCAL_PATH)/android \
-	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/video \
-        $(TOP)/frameworks/native/include/utils \
 	$(TOP)/system/core/include/system
+	
 LOCAL_MODULE := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_CFLAGS:= -DLOG_TAG=\"SPRDhwcomposer\"
-LOCAL_CFLAGS += -D_USE_SPRD_HWCOMPOSER -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES -w
-ifeq ($(strip $(TARGET_BOARD_PLATFORM)),sc8830)
-	LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libcamera/sc8830/inc	
-	# LOCAL_CFLAGS += -DVIDEO_LAYER_USE_RGB
-	# _HWCOMPOSER_USE_GSP : protecting sc8830 code
-	LOCAL_CFLAGS += -D_HWCOMPOSER_USE_GSP
-	# LOCAL_CFLAGS += -D_DMA_COPY_OSD_LAYER
-
-HWCOMPOSER_USE_GSP_BLEND := true
-ifeq ($(strip $(HWCOMPOSER_USE_GSP_BLEND)),true)
-	LOCAL_CFLAGS += -D_HWCOMPOSER_USE_GSP_BLEND
-else	
-	LOCAL_CFLAGS += -D_ALLOC_OSD_BUF
-endif
-
-endif
-
+LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES -w
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)),sc8825)
 	LOCAL_CFLAGS += -DSCAL_ROT_TMP_BUF
 
 	LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libcamera/sc8825/inc
 
 	LOCAL_SRC_FILES += sc8825/scale_rotate.c
+
 	LOCAL_CFLAGS += -D_PROC_OSD_WITH_THREAD
 
 	LOCAL_CFLAGS += -D_DMA_COPY_OSD_LAYER
 
 	LOCAL_CFLAGS += -D_ALLOC_OSD_BUF
+	LOCAL_CFLAGS += -D_SUPPORT_SYNC_DISP
 endif
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM)),sc8810)
 	LOCAL_CFLAGS += -DSCAL_ROT_TMP_BUF
-	LOCAL_SRC_FILES += sc8810/scale_rotate.c
-	LOCAL_CFLAGS += -D_SUPPORT_SYNC_DISP
-	LOCAL_CFLAGS += -D_VSYNC_USE_SOFT_TIMER
-endif
 
-ifeq ($(strip $(TARGET_BOARD_PLATFORM)),sc7710)
-	LOCAL_CFLAGS += -DSCAL_ROT_TMP_BUF
 	LOCAL_SRC_FILES += sc8810/scale_rotate.c
+
 	LOCAL_CFLAGS += -D_SUPPORT_SYNC_DISP
-	LOCAL_CFLAGS += -D_VSYNC_USE_SOFT_TIMER
 endif
 
 ifeq ($(strip $(USE_GPU_PROCESS_VIDEO)) , true)
@@ -90,31 +69,17 @@ endif
 else #android hwcomposer
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libEGL libutils libcutils
-LOCAL_SRC_FILES := vsync/vsync.cpp \
-                   android/hwcomposer.cpp \
+LOCAL_SHARED_LIBRARIES := liblog libEGL libcutils
+LOCAL_SRC_FILES := android/hwcomposer.cpp \
                    dump_bmp.cpp
 LOCAL_MODULE := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_C_INCLUDES := $(TOP)/frameworks/native/include/utils \
-		    $(LOCAL_PATH)/vsync \
-	            $(LOCAL_PATH)/android \
-                    $(LOCAL_PATH)/../gralloc \
+                    $(LOCAL_PATH)/android \
+                    $(LOCAL_PATH)/../libgralloc \
                     $(LOCAL_PATH)/../mali/src/ump/include
 LOCAL_CFLAGS:= -DLOG_TAG=\"hwcomposer\"
 
-ifeq ($(strip $(TARGET_BOARD_PLATFORM)),sc8810)
-	LOCAL_CFLAGS += -D_VSYNC_USE_SOFT_TIMER
-endif
-
-ifeq ($(strip $(TARGET_BOARD_PLATFORM)),sc7710)
-	LOCAL_CFLAGS += -D_VSYNC_USE_SOFT_TIMER
-endif
 endif
 
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-
-
