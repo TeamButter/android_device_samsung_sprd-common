c271c71080d975125ee505560d5863e7ceaffcf7
corphish <d97.avinaba@gmail.com>
2016-03-29 17:14:04 +0530

    device: Build libstagefrighthw
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ LOCAL_PATH := device/samsung/sprd-common
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
 	$(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
+	
+PRODUCT_PACKAGES += \
+	libstagefrighthw
 
 
 #Wifi
