8a1213c129bf72667cd5a0d578284205cae656e6
corphish <d97.avinaba@gmail.com>
2016-06-28 23:41:07 +0530

    common: (Yet again) Initial commit 3rd in this year :laugh:
    Signed-off-by: corphish <d97.avinaba@gmail.com>

---
+++ libsymbol_wrapper/VectorImpl.cpp
@@ -0,0 +1,22 @@
+
+#include <cstddef>
+
+/* void VectorImpl::reservedVectorImpl */
+extern "C" void _ZN7android10VectorImpl19reservedVectorImpl1Ev() { }
+extern "C" void _ZN7android10VectorImpl19reservedVectorImpl2Ev() { }
+extern "C" void _ZN7android10VectorImpl19reservedVectorImpl3Ev() { }
+extern "C" void _ZN7android10VectorImpl19reservedVectorImpl4Ev() { }
+extern "C" void _ZN7android10VectorImpl19reservedVectorImpl5Ev() { }
+extern "C" void _ZN7android10VectorImpl19reservedVectorImpl6Ev() { }
+extern "C" void _ZN7android10VectorImpl19reservedVectorImpl7Ev() { }
+extern "C" void _ZN7android10VectorImpl19reservedVectorImpl8Ev() { }
+
+/* void SortedVectorImpl::reservedSortedVectorImpl */
+extern "C" void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl1Ev() { }
+extern "C" void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl2Ev() { }
+extern "C" void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl3Ev() { }
+extern "C" void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl4Ev() { }
+extern "C" void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl5Ev() { }
+extern "C" void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl6Ev() { }
+extern "C" void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl7Ev() { }
+extern "C" void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl8Ev() { }
