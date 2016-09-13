cfa31a6d9d0e2cfac5eef7ff40a8a33d9df20243
corphish <d97.avinaba@gmail.com>
2016-09-13 12:32:26 +0530

    common: Build the hardware aspects of the phone
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- common.mk
+++ common.mk
@@ -15,6 +15,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/hardware/samsung/sprd/sprd.mk)
 
 LOCAL_PATH := device/samsung/sprd-common
 
